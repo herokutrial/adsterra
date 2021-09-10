e3424e741546b52fa12df686e270c7d024c1b83c
herokutrial <90392849+herokutrial@users.noreply.github.com>
2021-09-10 17:31:04 +0700

    Update directlink.js

--- directlink.js
+++ directlink.js
@@ -1,1 +1,10 @@
+$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+	{
+	    $(this).removeClass('xepo_ads');
 
+	    //ubah google dengan ads direct link
+	    window.open('https://www.effectivecpmgate.com/ep0c12kx8?key=533364bcee838f83aa428f9938260553', '_blank');
+	});
